7dd9ecb2e817c9686b8657089e23cfb462d9b236
Crumbls <chasecmiller@gmail.com>
2025-06-10 09:16:45 -0600

    Initial push by Chase.  Really just foundational.

---
+++ resources/lang/en/dispositions.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'opened' => 'Conversation started',
+    'closed' => 'Conversation closed',
+    'status_changed' => 'Status Changed from :from to :to',
+    'priority_changed' => 'Status Changed from :from to :to',
+    'turn_changed' => 'Status Changed from :from to :to',
+];
