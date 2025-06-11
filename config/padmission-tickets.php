2ee569a1dec94201c91a306526ac5f0ecf7a7256
Dennis Koch <info@pixelarbeit.de>
2025-06-11 18:37:40 +0200

    Add tenancy for Tickets

--- config/padmission-tickets.php
+++ config/padmission-tickets.php
@@ -4,6 +4,8 @@ use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\Facades\Config;
 
 return [
+    'run_migrations' => true,
+
     /**
      * Swap package models (left) with your own model (right).
      * Your models should extend the package models to ensure type safety.
@@ -16,6 +18,12 @@ return [
         Padmission\Tickets\Models\TicketActivity::class => Padmission\Tickets\Models\TicketActivity::class,
         Padmission\Tickets\Models\TicketPriority::class => Padmission\Tickets\Models\TicketPriority::class,
         Padmission\Tickets\Models\TicketStatus::class => Padmission\Tickets\Models\TicketStatus::class,
+    ],
+
+    'tenancy' => [
+        'enabled' => false,
+        'foreign_key' => 'tenant_id',
+        'foreign_key_type' => 'id', // options: 'id', 'ulid', 'uuid'
     ],
 
     'levels' => [
