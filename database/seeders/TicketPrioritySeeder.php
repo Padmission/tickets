cb91e66939a020b8277f4f7f3db2c95c2ada6546
Dennis Koch <info@pixelarbeit.de>
2025-06-12 21:59:57 +0200

    Improve seeders

--- database/seeders/TicketPrioritySeeder.php
+++ database/seeders/TicketPrioritySeeder.php
@@ -5,23 +5,30 @@ namespace Padmission\Tickets\Database\Seeders;
 use Filament\Facades\Filament;
 use Illuminate\Database\Seeder;
 use Padmission\Tickets\Models\TicketPriority;
+use Padmission\Tickets\TicketPlugin;
 
 class TicketPrioritySeeder extends Seeder
 {
     public function run(): void
     {
-        if (TicketPriority::query()->exists()) {
+        $priorityModel = TicketPlugin::resolveModelClass(TicketPriority::class);
+
+        if ($priorityModel::query()->exists()) {
             return;
         }
 
         foreach (Filament::getPanels() as $panel) {
             Filament::setCurrentPanel($panel);
 
-            TicketPriority::insert([
+            $data = [
                 ['display_name' => 'Low', 'panel' => $panel->getId(), 'color' => 'Blue', 'order' => 1],
                 ['display_name' => 'Medium', 'panel' => $panel->getId(), 'color' => 'Orange', 'order' => 2],
                 ['display_name' => 'High', 'panel' => $panel->getId(), 'color' => 'Red', 'order' => 3],
-            ]);
+            ];
+
+            foreach ($data as $row) {
+                $priorityModel::create($row);
+            }
         }
     }
 }
