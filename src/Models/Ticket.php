79f2f3ac6e7d3246604d1de7f9e8868ff749eb96
iAmKevinMcKee <4503765+iAmKevinMcKee@users.noreply.github.com>
2025-06-14 21:53:48 +0000

    Code style

--- src/Models/Ticket.php
+++ src/Models/Ticket.php
@@ -5,7 +5,6 @@ namespace Padmission\Tickets\Models;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Attributes\UseFactory;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
