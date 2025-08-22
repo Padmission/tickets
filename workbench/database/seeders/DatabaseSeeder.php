a3d5692271fc8db1c12d7a2e5fbfdacd9b586479
Dennis Koch <info@pixelarbeit.de>
2025-08-22 16:23:54 +0200

    Add TicketDispositionSeeder

--- workbench/database/seeders/DatabaseSeeder.php
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Padmission\Tickets\Database\Seeders\TicketDispositionSeeder;
 use Padmission\Tickets\Database\Seeders\TicketPrioritySeeder;
 use Padmission\Tickets\Database\Seeders\TicketSeeder;
 use Padmission\Tickets\Database\Seeders\TicketStatusSeeder;
@@ -24,6 +25,7 @@ class DatabaseSeeder extends Seeder
 
         (new TicketStatusSeeder)->run();
         (new TicketPrioritySeeder)->run();
+        (new TicketDispositionSeeder())->run();
         (new TicketSeeder)->run();
     }
 }
