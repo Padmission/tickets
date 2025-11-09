7122bbfa54795709aa5d6bf01d65b1170ea5c7f1
Dennis Koch <info@pixelarbeit.de>
2025-11-09 23:15:27 +0100

    Move docs button above create ticket button

--- resources/js/components/chat-widget/list.js
+++ resources/js/components/chat-widget/list.js
@@ -108,14 +108,6 @@ customElements.define(
                             ${__('list.subheading')}
                         </h3>
 
-                        <button
-                            class="button"
-                            @click="createTicket"
-                        >
-                            <span>${__('list.create_ticket')}</span>
-
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
-                        </button>
 
                         ${config.documentationUrl ?
                             `<a
@@ -128,6 +120,15 @@ customElements.define(
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
                             </a>` : ''
                         }
+
+                        <button
+                            class="button"
+                            @click="createTicket"
+                        >
+                            <span>${__('list.create_ticket')}</span>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
+                        </button>
 
                         <h3>${__('list.tickets_heading')}</h3>
 
