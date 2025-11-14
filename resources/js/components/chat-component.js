77a4734f1f34780584da92b72676aa9696c07061
Dennis Koch <info@pixelarbeit.de>
2025-11-14 20:34:52 +0100

    Remove shortcut indicator

--- resources/js/components/chat-component.js
+++ resources/js/components/chat-component.js
@@ -949,15 +949,6 @@ customElements.define(
                                 </svg>
 
                                 <span>${__('chat.send')}</span>
-
-                                <kbd>
-                                    <span class="sr-only">${__('chat.command_key')}</span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-command"><path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3"></path></svg>
-                                </kbd>
-                                <kbd>
-                                    <span class="sr-only">${__('chat.enter_key')}</span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-corner-down-left-icon lucide-corner-down-left"><path d="M20 4v7a4 4 0 0 1-4 4H4"/><path d="m9 10-5 5 5 5"/></svg>
-                                </kbd>
                             </button>
                         </div>
 
