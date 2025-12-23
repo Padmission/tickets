a6c996f85bec9a2213869ee765bb87d066b8ea0d
Dennis Koch <info@pixelarbeit.de>
2025-12-23 16:09:06 +0100

    Prefix dev routes

--- routes/dev.php
+++ routes/dev.php
@@ -9,34 +9,36 @@ use Padmission\Tickets\Models\Ticket;
 use Padmission\Tickets\Notifications\OtpNotification;
 use Padmission\Tickets\Notifications\TicketNotification;
 
-Route::get('/mail/created', function () {
-    $ticket = Ticket::first();
-    $user = User::first();
+Route::prefix('/dev/tickets')->group(function () {
+    Route::get('/mail/created', function () {
+        $ticket = Ticket::first();
+        $user = User::first();
 
-    return (new TicketNotification($ticket, new TicketCreatedEvent($ticket, $user)))->toMail($user);
-});
+        return (new TicketNotification($ticket, new TicketCreatedEvent($ticket, $user)))->toMail($user);
+    });
 
-Route::get('/mail/activity', function () {
-    $ticket = Ticket::first();
-    $user = User::first();
+    Route::get('/mail/activity', function () {
+        $ticket = Ticket::first();
+        $user = User::first();
 
-    return (new TicketNotification($ticket, new TicketActivityEvent($ticket, $user)))->toMail($user);
-});
+        return (new TicketNotification($ticket, new TicketActivityEvent($ticket, $user)))->toMail($user);
+    });
 
-Route::get('/mail/closed', function () {
-    $ticket = Ticket::first();
-    $user = User::first();
+    Route::get('/mail/closed', function () {
+        $ticket = Ticket::first();
+        $user = User::first();
 
-    return (new TicketNotification($ticket, new TicketClosedEvent($ticket, $user)))->toMail($user);
-});
+        return (new TicketNotification($ticket, new TicketClosedEvent($ticket, $user)))->toMail($user);
+    });
 
-Route::get('/mail/assigned', function () {
-    $ticket = Ticket::first();
-    $user = User::first();
+    Route::get('/mail/assigned', function () {
+        $ticket = Ticket::first();
+        $user = User::first();
 
-    return (new TicketNotification($ticket, new TicketAssignedEvent($ticket, $user)))->toMail($user);
-});
+        return (new TicketNotification($ticket, new TicketAssignedEvent($ticket, $user)))->toMail($user);
+    });
 
-Route::get('/mail/otp', function () {
-    return (new OtpNotification(Ticket::first(), 'closed'))->toMail(User::first());
+    Route::get('/mail/otp', function () {
+        return (new OtpNotification(Ticket::first(), 'closed'))->toMail(User::first());
+    });
 });
